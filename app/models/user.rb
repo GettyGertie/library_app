2ba39c80fdbe540b746801762d52dc655b06779e
GettyGertie <gettyorawo@gmail.com>
2016-07-11 13:36:27 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
-class User < ApplicationRecord
-  validates :name, presence: true, length: {maximum: 5}
-  validates :email, presence: true, length: {maximum: 51 }
-
-end
+class User < ApplicationRecord 
+before_save { self.email = email.downcase  }
+validates :name, presence: true, length: {maximum: 20} 
+EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true, length: {maximum: 51 }, 
+          format: { with: EMAIL_REGEX },
+          uniqueness: {case_sensitive: false}
+ end 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 def setup
-  @user = User.new(name: "Example" , email: "example@email.com")
+  @user = User.new(name: "Example" ,
+                   email: "example@email.com")
 end
 
 test "user should be valid" do
   #should be assert and not assert_not below
-  assert_not @user.valid?
+  assert @user.valid?
 end
 
 test "name should be present" do
@@ -16,21 +17,33 @@ test "name should be present" do
 end
 
 test "name should not be too long" do
-  @user.name = "tetereenterreennn"
+  @user.name = "it" * 11
   assert_not @user.valid?
 end
 
-# test "email should be present" do
-#   @user.email = " "
-#   assert_not @user.valid?
-# end
-
-# test "email should not be too long" do
-#   @user.email = "email"* 99 
-#   assert_not @user.valid?
-#  end
-  test "password should have a minimum length" do
-  @user.password = @user.password_confirm = "a" * 5
+test "email should be present" do
+  @user.email = " "
   assert_not @user.valid?
-  end
+end
+
+
+test "email should not be too long" do
+  @user.email = "email"* 99 
+  assert_not @user.valid?
+ end
+
+test "should accept valid email adresses" do
+valid_email = %w[example@gmail.com EXAMPLE@gmail.com ex_am_ple@gmail.com exa+mple@gmail.com]
+valid_email.each do |valid_emails|
+  @user.email = valid_emails
+  assert @user.valid?
+end
+end
+
+
+test "email should be unique" do
+  dup_user = @user.dup
+  dup_user.email = @user.email.upcase
+  assert_not dup_user.valid?
+end
 end
