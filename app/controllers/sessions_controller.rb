a47bb93dd8914e816adb806c99af9553199048ba
GettyGertie <gettyorawo@gmail.com>
2016-07-12 19:22:44 +0300

    Test flash.now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # log_in user
       # redirect_to user
     else
-      flash.now[:danger] = "Invalid email and password combination!
-                        Please try again ..."
+      flash.now[:danger] = 'Invalid email and password combination!
+                        Please try again ...'
 
       render 'new'
     end
