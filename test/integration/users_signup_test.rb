4c4b124fd67557ff19e1ca412be79e96fd9e3f6b
GettyGertie <gettyorawo@gmail.com>
2016-07-12 22:02:08 +0300

    Tidy code in users signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,8 +6,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       post users_path, user: { name: "",
                                email: "user@invalid",
-                               password:
-                               "foo",
+                               password: "foo",
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
@@ -18,8 +17,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: { name: "Example User",
                                             email: "user@example.com",
-                                            password:
-                                            "password",
+                                            password: "password", 
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
