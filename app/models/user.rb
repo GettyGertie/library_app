262a6129d1db19593bade2704af6bb293f518e57
GettyGertie <gettyorawo@gmail.com>
2016-07-12 21:45:22 +0300

    Define user digest in user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ validates :email, presence: true, length: {maximum: 51 },
           has_secure_password
           validates :password, length: { minimum: 6  }
 
+
 def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
     BCrypt::Engine.cost
