f57a135b03ab0910fe8642a6972bd563e08de230
GettyGertie <gettyorawo@gmail.com>
2016-07-13 19:11:55 +0300

    Define forget in sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,5 +30,11 @@ end
     session.delete(:user_id)
     @current_user = nil
   end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
   
 end
