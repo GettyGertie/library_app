102949536a13772e8e26fa032b690f46702e4e51
GettyGertie <gettyorawo@gmail.com>
2016-07-13 19:22:52 +0300

    Define authenticated to return false if remember digest is nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,7 @@ BCrypt::Password.create(string, cost: cost)
     end
 
     def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
 
