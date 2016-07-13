84d898c86d9d0fe6bacf4487290333d36eacbb71
GettyGertie <gettyorawo@gmail.com>
2016-07-13 19:35:20 +0300

    Test login with remembering

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -45,4 +45,9 @@ test "login with valid information followed by logout" do
     count: 0
   assert_select "a[href=?]", user_path(@user), count: 0
 end
+
+test "login with remembering" do
+  log_in_as(@user, remember_me: '1')
+  assert_not_nil cookies['remember_token']
+end
 end
