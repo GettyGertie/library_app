6aa434cdd80246a24853513cad48a16ae89b1e6b
GettyGertie <gettyorawo@gmail.com>
2016-07-14 21:44:58 +0300

    Define logged_in_user in private users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
 
+      def logged_in_user
+        unless logged_in?
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
+      end
   end
 
 end
