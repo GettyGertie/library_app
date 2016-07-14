9d3f553445d1c77fc6502f0cb146b5c11cd8a5e8
GettyGertie <gettyorawo@gmail.com>
2016-07-14 22:00:17 +0300

    Redirect update when not logged in test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,4 +11,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email  }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
