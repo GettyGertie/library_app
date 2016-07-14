b9723d9367a3ce76123b7a53f96394c67af895fe
GettyGertie <gettyorawo@gmail.com>
2016-07-14 21:24:40 +0300

    Unsuccessful update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   end
 
   def update
+
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       # Handle a successful update.
