c2e321b5e33004b30b45b7ebf7563a02341b952a
GettyGertie <gettyorawo@gmail.com>
2016-07-16 12:34:21 +0300

    Add end to strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-
+   end
       def logged_in_user
         unless logged_in?
           flash[:danger] = "Please log in."
@@ -45,4 +45,3 @@ class UsersController < ApplicationController
       end
   end
 
-end
