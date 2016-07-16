3595d445bcf667012d7e9d3c06867d9718c4f960
GettyGertie <gettyorawo@gmail.com>
2016-07-16 16:59:09 +0300

    Add length validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  validate :title, presence: true
-  validate :author,presence: true
-  validate :description, presence: true
+  validates :title, presence: true, length: {maximum: 30}
+  validates :author,presence: true, length: {maximum: 50}
+  validates :description, presence: true, length: {maximum: 500}
 end
