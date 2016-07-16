e60a2d24cdecb13bb4cbe9ef89ef7757826e8df5
GettyGertie <gettyorawo@gmail.com>
2016-07-16 16:36:04 +0300

    Validate author presence

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   validate :title, presence: true
-  #validate :author,presence: true
+  validate :author,presence: true
   validate :description, presence: true
 end
