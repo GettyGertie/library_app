6f6c7a3f223ca498c0999e90946ac887eea72d93
GettyGertie <gettyorawo@gmail.com>
2016-07-19 18:57:38 +0300

    Remove deprication warnings

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -8,7 +8,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, session: { email: "", password: ""  }
+    post login_path, params:{session: { email: "", password: ""  }}
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
@@ -16,7 +16,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 test "login with valid information" do
   get login_path
-  post login_path, session: { email: @user.email, password: 'password'  }
+  post login_path, params:{session: { email: @user.email, password: 'password'  }}
   assert_redirected_to @user
   follow_redirect!
   assert_template 'users/show'
@@ -27,7 +27,7 @@ test "login with valid information" do
 
 test "login with valid information followed by logout" do
   get login_path
-  post login_path, session: { email: @user.email, password: 'password'  }
+  post login_path, params:{session: { email: @user.email, password: 'password'  }}
   assert is_logged_in?
   assert_redirected_to @user
   follow_redirect!
