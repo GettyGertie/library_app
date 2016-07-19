4b51f0f44e1b87ef7d9e5d18956e09a67e9acdc8
GettyGertie <gettyorawo@gmail.com>
2016-07-19 18:55:16 +0300

    Fixed  bugs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,13 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
+ # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.account_activation.subject
-
+  #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "Account Activation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,6 +17,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
-     end
+    mail to: user.email, subject: "Password Reset"
+  end
 end
