4f576946669baae27d407d1b04dcc4233ece8206
GettyGertie <gettyorawo@gmail.com>
2016-07-19 14:58:52 +0300

    Finished password resets

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,18 +11,4 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_response :success
   end
-
-  test "should redirect destroy when not logged in" do
-    assert_no_difference 'User.count' do
-      delete :destroy, id: @user
-    end
-    assert_redirected_to login_url
-  end
-  test "should redirect destroy when logged in as a non-admin" do
-    log_in_as(@other_user)
-    assert_no_difference 'User.count' do
-      delete :destroy, id: @user
-    end
-    assert_redirected_to root_url
-  end
 end
