e17f70e16853b30eee815ab74698d5d3f8dfd27b
GettyGertie <gettyorawo@gmail.com>
2016-07-19 18:55:42 +0300

    Fix errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,10 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+    # In the development environment your application's code is reloaded on
+    # every request. This slows down response time but is perfect for development
+    # since you don't have to restart the web server when you make code changes.
+    config.cache_classes = false
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -26,11 +26,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
+  # The mailer should be able to send.
   config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host  }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -54,3 +56,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+#                                 }
