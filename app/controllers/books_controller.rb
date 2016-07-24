66a820d9e10f0c11908526aa35c78409b307b7a7
GettyGertie <gettyorawo@gmail.com>
2016-07-24 13:15:36 +0300

    Include actions in admin's before filter

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user, only: [:create, :edit, :update]
-  before_action :admin_user, only: [:borrowed_books]
+  before_action :admin_user, only: [:borrowed_books, :return, :given_away_books, :give_book]
   
   def new
     @book = Book.new
